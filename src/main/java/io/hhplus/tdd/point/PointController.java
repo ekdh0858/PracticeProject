fd8703ea02c395230305c4df7d9e64adb7e6054f
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2025-08-16 02:43:57 +0900

    필수과제 - 1.TODO 기본 기능 작성
    1. GET /point/{id}
    2. GET /point/{id}/histories
    3. PATCH /point/{id}/use
    4. PATCH  /point/{id}/charge

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +9,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/point")
+@RequiredArgsConstructor
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -19,7 +22,8 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("{}의 포인트 조회", id);
+        return pointService.getUserPointById(id);
     }
 
     /**
@@ -29,7 +33,8 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        log.info("{}의 포인트 내역 조회", id);
+        return pointService.getAllHistoryById(id);
     }
 
     /**
@@ -40,7 +45,8 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("{}의 {}포인트 충전", id,amount);
+        return pointService.chargePoint(id, amount);
     }
 
     /**
@@ -51,6 +57,7 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("{}의 {}포인트 사용", id,amount);
+        return pointService.usePoint(id, amount);
     }
 }
